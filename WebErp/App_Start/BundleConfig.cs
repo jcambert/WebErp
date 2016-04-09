7947cd8e25f40e20a3694235f0ecc19c170085cc
jcambert <jc.ambert@free.fr>
2016-04-09 23:25:29 +0200

    First Commit

--- WebErp/App_Start/BundleConfig.cs
+++ WebErp/App_Start/BundleConfig.cs
@@ -8,8 +8,17 @@ namespace WebErp
         // Pour plus d’informations sur le regroupement, rendez-vous sur http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+
+
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery")
+                .Include("~/Scripts/jquery-{version}.js")
+                .Include("~/Scripts/jquery.signalR-{version}.js")
+            );
+
+            bundles.Add(new ScriptBundle("~/bundles/weberp")
+                .Include("~/Scripts/app/weberp.js","~/Scripts/app/weberp.config.js","~/Scripts/app/weberp.service.js","~/Scripts/app/weberp.directive.js")
+            );
 
             // Utilisez la version de développement de Modernizr pour développer et apprendre. Puis, lorsque vous êtes
             // prêt pour la production, utilisez l’outil de génération sur http://modernizr.com pour sélectionner uniquement les tests dont vous avez besoin.
@@ -22,10 +31,23 @@ namespace WebErp
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/ui-bootstrap-csp.css",
+                      "~/Content/font-awesome.css",
                       "~/Content/site.css"));
 
             bundles.Add(new ScriptBundle("~/bundles/angular").Include(
-                "~/Scripts/angular.js"));
+                "~/Scripts/angular.js",
+                "~/SCripts/angular-animate.js",
+                "~/Scripts/angular-cookies.js",
+                "~/SCripts/angular-resource.js",
+                "~/Scripts/angular-ui-router.js",
+                "~/Scripts/angular-ui/ui-bootstrap.js",
+                "~/Scripts/angular-ui/ui-bootstrap.tpls.js"
+                ));
+
+            bundles.Add(new ScriptBundle("~/bundles/lodash")
+                .Include("~/Scripts/lodash.js"
+                ));
         }
     }
 }
